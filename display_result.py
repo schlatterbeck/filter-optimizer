1d367ba5073671cf9fbd4c2c01670cddf18d7546
Ralf Schlatterbeck <rsc@runtux.com>
2020-09-05 19:30:35 +0200

    Fix labels and title

--- display_result.py
+++ display_result.py
@@ -6,6 +6,7 @@ import numpy as np
 from csv      import DictReader
 from math     import sqrt
 from argparse import ArgumentParser
+from numbers  import Number
 
 class Eval_Data :
 
@@ -15,6 +16,27 @@ class Eval_Data :
         with open (args.filename, 'r')  as f :
             dr = DictReader (f, delimiter = ';')
             key_attributes = dr.fieldnames [:5]
+            x_axis = None
+            self.x_idx = None
+            keys = []
+            for i, name in enumerate (key_attributes) :
+                arg = getattr (args, name)
+                if  (  arg is None
+                    or arg == ''
+                    or isinstance (arg, Number) and arg < 0
+                    ) :
+                    if x_axis is None :
+                        x_axis = name
+                        self.x_idx  = i
+                        self.x_name = name
+                    else :
+                        raise ValueError \
+                            ('(More than) two key attributes used for X: %s, %s'
+                            % (name, arg)
+                            )
+                else :
+                    keys.append (name)
+            self.keys = keys = tuple (keys)
             for rec in dr : 
                 rec ['np']    = int (rec ['np'])
                 rec ['F']     = float (rec ['F'])
@@ -25,7 +47,7 @@ class Eval_Data :
                 rec ['iter']  = int (rec ['iter'])
 
                 do_continue = False
-                for k in key_attributes :
+                for k in keys :
                     arg = getattr (args, k)
                     if arg and arg != rec [k] :
                         do_continue = True
@@ -68,14 +90,14 @@ class Eval_Data :
         nev  = []
         xmin = 1e6
         xmax = -1
-        for k in sorted (rbk, key = lambda x : int (x [2])) :
+        for k in sorted (rbk, key = lambda x : x [self.x_idx]) :
             r = rbk [k]
-            n = int (k [2])
-            x.append (n)
-            if n < xmin :
-                xmin = n
-            if n > xmax :
-                xmax = n
+            a = k [self.x_idx]
+            x.append (a)
+            if a < xmin :
+                xmin = a
+            if a > xmax :
+                xmax = a
             y2.append ((r ['success']) / (r ['success'] + r ['fail']) * 100)
             if r ['success'] :
                 y1.append (r ['mean'])
@@ -85,11 +107,18 @@ class Eval_Data :
                 nev.append (np.array ([]))
         nev = np.array (nev)
 
-        plt.title  ('Evaluations, Successes')
-        plt.xlabel ('NP')
-        bp = ax1.boxplot (nev / 1000, positions = x, widths = 10)
+        plt.title \
+            ( 'Evaluations, Successes\n'
+            + ' '.join
+                ( '='.join ((k, str (getattr (self.args, k))))
+                  for k in self.keys
+                )
+            )
+        plt.xlabel (self.x_name)
+        tick = (xmax - xmin) / len (rbk) / 2.
+        bp = ax1.boxplot (nev / 1000, positions = x, widths = tick)
         plt.ylabel ('Evals (thousands)', color = bp ['medians'][0].get_color ())
-        ax1.set_xlim (xmin - 10, xmax + 10, auto = True)
+        ax1.set_xlim (xmin - tick / 2, xmax + tick / 2, auto = True)
         ax2 = ax1.twinx ()
         p, = ax2.plot   (x, y2)
         plt.ylabel ('Successes (%)', color = p.get_color ())
@@ -126,7 +155,7 @@ def main () :
         , type    = int
         , help    = "Population size, default=%(default)s"
         , dest    = 'np'
-        , default = 0
+        , default = None
         )
     cmd.add_argument \
         ( '-s', '--sort-population'
