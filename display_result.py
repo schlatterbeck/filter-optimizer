faef389ffe4bc6eef7f61da805387b733d251d13
Ralf Schlatterbeck <rsc@runtux.com>
2020-09-05 10:38:30 +0200

    Re-factor result display

--- display_result.py
+++ display_result.py
@@ -1,79 +1,144 @@
 #!/usr/bin/python3
 
 import sys
-from csv  import DictReader
-from math import sqrt
 import matplotlib.pyplot as plt
 import numpy as np
+from csv      import DictReader
+from math     import sqrt
+from argparse import ArgumentParser
 
-def extract_data (key) :
-    assert key [0] == 'best'
-    assert key [1] == 'bin'
-    assert key [4] == '0'
-    assert float (key [3]) == 0.85
-    return int (key [2])
-# end def extract_data
+class Eval_Data :
 
-def plot_neval (rbk) :
-    fig  = plt.figure ()
-    ax1  = fig.add_subplot (111)
-    x    = []
-    y1   = []
-    y2   = []
-    yerr = []
-    nev  = []
-    xmin = 1e6
-    xmax = -1
-    for k in sorted (rbk, key = lambda x : int (x [2])) :
-        r = rbk [k]
-        if 'mean' not in r :
-            continue
-        n = extract_data (k)
-        x.append (n)
-        if n < xmin :
-            xmin = n
-        if n > xmax :
-            xmax = n
-        y1.append (r ['mean'])
-        y2.append ((r ['success']) / (r ['success'] + r ['fail']) * 100)
-        nev.append (r ['neval'])
-    nev = np.array (nev)
-    bp = ax1.boxplot (nev / 1000, positions = x, widths = 10)
-    plt.title  ('Evaluations, Successes')
-    plt.ylabel ('Evals (thousands)', color = bp ['medians'][0].get_color ())
-    plt.xlabel ('NP')
-    ax1.set_xlim (xmin - 10, xmax + 10, auto = True)
-    ax2 = ax1.twinx ()
-    p, = ax2.plot   (x, y2)
-    plt.ylabel ('Successes (%)', color = p.get_color ())
-    plt.show ()
-# end def plot_neval
+    def __init__ (self, args) :
+        self.args = args
+        result_by_key = {}
+        with open (args.filename, 'r')  as f :
+            dr = DictReader (f, delimiter = ';')
+            key_attributes = dr.fieldnames [:5]
+            for rec in dr : 
+                rec ['np']    = int (rec ['np'])
+                rec ['F']     = float (rec ['F'])
+                rec ['sort']  = int (rec ['sort'])
+                rec ['idx']   = int (rec ['idx'])
+                rec ['eval']  = float (rec ['eval'])
+                rec ['neval'] = int (rec ['neval'])
+                rec ['iter']  = int (rec ['iter'])
 
-result_by_key = {}
-with open (sys.argv [1], 'r')  as f :
-    dr = DictReader (f, delimiter = ';')
-    key_attributes = dr.fieldnames [:5]
-    for rec in dr : 
-        key = tuple (rec [k] for k in key_attributes)
-        if key not in result_by_key :
-            result_by_key [key] = dict \
-                ( success = 0
-                , fail    = 0
-                , eval    = []
-                , neval   = []
-                )
-        if float (rec ['eval']) == 0 :
-            result_by_key [key]['success'] += 1
-            result_by_key [key]['neval'].append (int (rec ['neval']))
-        else :
-            result_by_key [key]['fail'] += 1
-            result_by_key [key]['eval'].append (float (rec ['eval']))
-for k in result_by_key :
-    r = result_by_key [k]
-    r ['eval']  = np.array (r ['eval'])
-    r ['neval'] = np.array (r ['neval'])
-    n = r ['success']
-    if n :
-        r ['mean'] = sum (r ['neval']) / n
-        r ['stdd']  = sqrt (sum ((r ['neval'] - r ['mean']) ** 2)) / n
-plot_neval (result_by_key)
+                do_continue = False
+                for k in key_attributes :
+                    arg = getattr (args, k)
+                    if arg and arg != rec [k] :
+                        do_continue = True
+                        break
+                if do_continue :
+                    continue
+                key = tuple (rec [k] for k in key_attributes)
+                if key not in result_by_key :
+                    result_by_key [key] = dict \
+                        ( success = 0
+                        , fail    = 0
+                        , eval    = []
+                        , neval   = []
+                        )
+                if rec ['eval'] == 0 :
+                    result_by_key [key]['success'] += 1
+                    result_by_key [key]['neval'].append (rec ['neval'])
+                else :
+                    result_by_key [key]['fail'] += 1
+                    result_by_key [key]['eval'].append (rec ['eval'])
+        for k in result_by_key :
+            r = result_by_key [k]
+            r ['eval']  = np.array (r ['eval'])
+            r ['neval'] = np.array (r ['neval'])
+            n = r ['success']
+            if n :
+                r ['mean'] = sum (r ['neval']) / n
+                r ['stdd'] = sqrt (sum ((r ['neval'] - r ['mean']) ** 2)) / n
+        self.result_by_key = result_by_key
+    # end def __init__
+
+    def plot_eval_success (self) :
+        rbk  = self.result_by_key
+        fig  = plt.figure ()
+        ax1  = fig.add_subplot (111)
+        x    = []
+        y1   = []
+        y2   = []
+        yerr = []
+        nev  = []
+        xmin = 1e6
+        xmax = -1
+        for k in sorted (rbk, key = lambda x : int (x [2])) :
+            r = rbk [k]
+            n = int (k [2])
+            x.append (n)
+            if n < xmin :
+                xmin = n
+            if n > xmax :
+                xmax = n
+            y2.append ((r ['success']) / (r ['success'] + r ['fail']) * 100)
+            if r ['success'] :
+                y1.append (r ['mean'])
+                nev.append (r ['neval'])
+            else :
+                y1.append (0)
+                nev.append (np.array ([]))
+        nev = np.array (nev)
+
+        plt.title  ('Evaluations, Successes')
+        plt.xlabel ('NP')
+        bp = ax1.boxplot (nev / 1000, positions = x, widths = 10)
+        plt.ylabel ('Evals (thousands)', color = bp ['medians'][0].get_color ())
+        ax1.set_xlim (xmin - 10, xmax + 10, auto = True)
+        ax2 = ax1.twinx ()
+        p, = ax2.plot   (x, y2)
+        plt.ylabel ('Successes (%)', color = p.get_color ())
+        plt.show ()
+    # end def plot_eval_success
+
+# end class Eval_Data
+
+def main () :
+    cmd = ArgumentParser ()
+    cmd.add_argument \
+        ( 'filename'
+        , help    = "File name of CSV result data"
+        )
+    cmd.add_argument \
+        ( '-d', '--variant'
+        , help    = "Variant of DE, one of rand/best default=%(default)s"
+        , default = 'best'
+        )
+    cmd.add_argument \
+        ( '-c', '--cross'
+        , help    = "Crossover type, one of bin/exp, default=%(default)s"
+        , default = 'bin'
+        )
+    cmd.add_argument \
+        ( '-F', '--scale-factor'
+        , help    = "Base DE scale factor F, default=%(default)s"
+        , default = 0.85
+        , dest    = 'F'
+        , type    = float
+        )
+    cmd.add_argument \
+        ( '-p', '--popsize'
+        , type    = int
+        , help    = "Population size, default=%(default)s"
+        , dest    = 'np'
+        , default = 0
+        )
+    cmd.add_argument \
+        ( '-s', '--sort-population'
+        , help    = "Sort population by angle/radius"
+        , default = False
+        , dest    = 'sort'
+        , action  = 'store_true'
+        )
+    args = cmd.parse_args ()
+    ed = Eval_Data (args)
+    ed.plot_eval_success ()
+# end def main
+
+if __name__ == '__main__' :
+    main ()
